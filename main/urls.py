8e0e6287e49c8499c6e38b0aa87740d2fe9d9fa5
unknown <kaustubhkrishna9031@gmail.com>
2019-08-05 18:47:45 +0530

    final 05082019

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^login$', views.login_user, name='login'),
-    url(r'^home$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^logout$', views.log_out, name='logout')
 ]
