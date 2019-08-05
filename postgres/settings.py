88edcc6483874237a9e7eb4136e865aaea3e0274
unknown <kaustubhkrishna9031@gmail.com>
2019-08-05 17:52:29 +0530

    heroku

--- postgres/settings.py
+++ postgres/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,5 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/home'
+
+django_heroku.settings(locals())
