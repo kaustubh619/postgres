517d30b641e193ea6e0e8694c385f6a950b0ff9d
unknown <kaustubhkrishna9031@gmail.com>
2019-08-06 00:23:00 +0530

    rtyrtyasd

--- postgres/settings.py
+++ postgres/settings.py
@@ -30,16 +30,17 @@ DEBUG = True
 ACCOUNT_EMAIL_REQUIRED = True
 
 ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # 1 day
-# ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400
 
 # or any other page
-# ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/login'
 
 # redirects to profile page if not configured.
-# LOGIN_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/home'
 
 INSTALLED_APPS = [
     'main',
