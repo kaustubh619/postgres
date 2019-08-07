6257fe9a47b8f8597f1b303adc60735c84d750f8
unknown <kaustubhkrishna9031@gmail.com>
2019-08-07 21:48:10 +0530

    email

--- postgres/settings.py
+++ postgres/settings.py
@@ -152,13 +152,14 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kaustubhkrishna9031@gmail.com'
