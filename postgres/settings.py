44be0aabc14288ece2c1695a71d8a189086b9dc5
unknown <kaustubhkrishna9031@gmail.com>
2019-08-07 22:41:07 +0530

    pass

--- postgres/settings.py
+++ postgres/settings.py
@@ -163,7 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kaustubhkrishna9031@gmail.com'
-EMAIL_HOST_PASSWORD = 'kaustubh_911'
+EMAIL_HOST_PASSWORD = 'wtwwrelkkdtdcguk'
 EMAIL_PORT = 587
 SERVER_EMAIL = 'kaustubhkrishna9031@gmail.com'
 
